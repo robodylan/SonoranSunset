6568a70b1400ec5d7f10d8450a7aeb67314bb039
Dylan Dunn <robodylan123@gmail.com>
2016-08-11 10:14:01 -0400

    Primary project structure
    Layed basic files and set standards for organization

---
+++ SonoranSunset/Scene/Scene.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SonoranSunset.Scene
+{
+    class Scene
+    {
+        public static Scene current;
+
+        public int time;
+        public Color SkyColor;
+        public Scene()
+        {
+
+        }
+    }
+}
